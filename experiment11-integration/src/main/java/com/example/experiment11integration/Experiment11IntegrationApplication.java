dc041e575c1df372044d7a299d7753d7db5c453a
chong <304451676@qq.com>
2019-05-19 23:31:10 +0800

    前后端整合项目--后端部分

---
+++ experiment11-integration/src/main/java/com/example/experiment11integration/Experiment11IntegrationApplication.java
@@ -0,0 +1,28 @@
+package com.example.experiment11integration;
+
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+public class Experiment11IntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Experiment11IntegrationApplication.class, args);
+    }
+    // 注入密码编码组件
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    // 序列化延迟加载对象的ID
+    @Bean
+    protected Hibernate5Module module() {
+        Hibernate5Module module = new Hibernate5Module();
+        module.enable(Hibernate5Module.Feature.SERIALIZE_IDENTIFIER_FOR_LAZY_NOT_LOADED_OBJECTS);
+        return module;
+    }
+}
---
+++ experiment11-integration/src/test/java/com/example/experiment11integration/Experiment11IntegrationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.experiment11integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Experiment11IntegrationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
